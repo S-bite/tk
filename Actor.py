f6df9ff34a7f09c246487062d151896d6b265ab8
bitbite <0125bitbite@gmail.com>
2017-05-24 12:49:34 +0900

    modified:   Actor.py 	modified:   ActorController.py 	deleted:    Actor.py.bak 	deleted:    ActorController.py.bak 	deleted:    main.py.bak 	deleted:    pygameWrapper.py.bak 	deleted:    util.py.bak 	deleted:    variable.py.bak 	deleted:    world.py.bak

--- Actor.py
+++ Actor.py
@@ -1,10 +1,4 @@
 #coding:utf-8
-from world import *
-from util import *
-from variable import (char)
-from pygame.locals import *
-from pygameWrapper import stage
-import time
 
 """
 Actor:
@@ -21,105 +15,42 @@ STR
 DEF
 SPD
 ...
-これから増やす
-
-パラメーターを増やす、減らす
-
-monster=Actor(SOME_MONSTER,[31,11])
+数値は辞書形式で格納
+param={HP=10,STR=3...}
+foo=Actor(param)
 """
-mapX, mapY = 640 / 32, 480 / 32
 class Actor():
-    def __init__(self,ActorData=None,pos=None): # default argument value is mutable
-        if ActorData==None:
-            ActorData=[0,"None",0,"None","None",0,0,0,0,0,[]]
-        if pos==None:
-            pos=[0,0]
-        self.act_state = -1
-        self.id    = ActorData[0]
-        self.name  = ActorData[1]
-        self.image = ActorData[2]
-        self.race  = ActorData[3]
-        self.job   = ActorData[4]
-        self.HP_MAX    = ActorData[5]
-        self.HP = ActorData[5]
-        self.MP_MAX    = ActorData[6]
-        self.MP = ActorData[5]
-        self.STR   = ActorData[7]
-        self.DEF   = ActorData[8]
-        self.SPD   = ActorData[9]
-        self.state = ActorData[10]
-        self.x = pos[0]
-        self.y = pos[1]
+    def __init__(self,data={}): # default argument value is mutable
 
-        char[self.y][self.x] = self.image
+        defaultData={"act_state":-1,"act_id":-1,"name":"-1","image":-1,"race":"-1","job":"-1","HP_MAX":-1,"HP":-1,
+            "MP_MAX":-1,"MP":-1,"STR":-1,"DEF":-1,"SPD":-1,"state":-1,"x":-1,"y":-1}
+        defaultData.update(data)
+        data=defaultData
+        for d in data:
+            setattr(self,d,data[d])
+
+        checkDict=["act_state","act_id","name","image","race","job","HP_MAX","HP","MP_MAX","MP","STR","DEF","SPD","state",
+                "x","y"]
+        if [hasattr(self,x) and str(type(getattr(self,x)))!="<class 'function'>" for x in checkDict] .count(False)!=0:
+            raise ("necessary variable is not defined")
+
 class Enemy(Actor):
-    def action(self,target):
-        char[self.y][self.x]=-1
-        def f(i):
-            if i==0:
-                return 1
-            return i
-        yMove = (target.y - self.y) // abs(f(target.y - self.y))
-        xMove = (target.x - self.x) // abs(f(target.x - self.x))
-        if is_movalbe(self.y+yMove,self.x+xMove)==0:
-            self.x +=xMove
-            self.y +=yMove
-        char[self.y][self.x]=self.image
+        def __init__(self,data={}):
+            defaultData = {"skill":["-1"]}
+            defaultData.update(data)
+            data=defaultData
+            Actor.__init__(self,data)
+
+            checkDict = ["skill"]
+            if [hasattr(self, x) and str(type(getattr(self, x))) != "<class 'function'>" for x in checkDict].count(
+                    False) != 0:
+                raise (BaseException("necessary variables are not defined"))
 
         pass
 
 class Player(Actor):
-    def action(self,key):
-        return self.move(key)
-    def move(self,key):
-        stage.drawChips(cutWorldMapToDisplay(worldMap, self.x, self.y, mapX, mapY), source="field")
-        stage.drawChips(cutWorldMapToDisplay(char, self.x, self.y, mapX, mapY), "char")
-        xMove, yMove = 0, 0
+    pass
 
-        if key[K_UP]:
-            yMove = -1
-        if key[K_DOWN]:
-            yMove = 1
-        if key[K_RIGHT]:
-            xMove = 1
-        if key[K_LEFT]:
-            xMove = -1
-        res=is_movalbe(self.y+yMove,self.x+xMove)
-        if  res==2:
-          #  time.sleep(0.1)
-            stage.appendLog("その方向には進めない")
-            return False
-        if res==1:
-            """
-            attack
-            """
-            res=self.attack(getIdFromPos(self.x+xMove,self.y+yMove))
-            stage.appendLog("敵に"+str(res)+"のダメージを与えた")
-            time.sleep(0.1)
-            return False
-        char[self.y][self.x] = -1
-        self.x += xMove
-        self.y += yMove
-        if xMove != 0 or yMove != 0:
-            stage.appendLog(str(self.name)+"は" + str(self.x) + "," + str(self.y) + "へ移動した")
-        char[self.y][self.x] = self.image
-        #stage.drawChips(cutWorldMapToDisplay(worldMap, self.x, self.y, mapX, mapY), source="field")
-        #stage.drawChips(cutWorldMapToDisplay(char, self.x, self.y, mapX, mapY), "char")
-
-        #time.sleep(0.1)
-        return True
-    def attack(self,target):#,targetはid
-
-        for actor in actors:
-            if actor.id == target:
-                damage=int((self.STR*(0.99)**actor.DEF))+random.randint(1,20)
-                actor.HP-=damage
-                if actor.HP<0:
-                    actors.remove(actor)
-                    char[actor.y][actor.x]=-1
-                    stage.drawChips(cutWorldMapToDisplay(worldMap, self.x, self.y, mapX, mapY), source="field")
-                    stage.drawChips(cutWorldMapToDisplay(char, self.x, self.y, mapX, mapY), "char")
-                    stage.update()
-                return damage
-        return -1
-
+param={"HP_MAX":1,"HP":1111111}
+s=Enemy(param)
+print(s.HP)
