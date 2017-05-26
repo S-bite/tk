eff5f22e991a747fbf5ee70cf5412a65d2eb2f91
bitbite <0125bitbite@gmail.com>
2017-05-26 12:16:44 +0900

    modified:   Actor.py 	modified:   ActorController.py 	new file:   tk_flowchart.xml 	deleted:    Untitled Diagram.xml

--- Actor.py
+++ Actor.py
@@ -22,26 +22,25 @@ foo=Actor(param)
 class Actor():
     def __init__(self,data={}): # default argument value is mutable
 
-        defaultData={"act_state":-1,"act_id":-1,"name":"-1","image":-1,"race":"-1","job":"-1","HP_MAX":-1,"HP":-1,
+        defaultData={"actState":-1,"actId":-1,"name":"-1","image":-1,"race":"-1","job":"-1","HP_MAX":-1,"HP":-1,
             "MP_MAX":-1,"MP":-1,"STR":-1,"DEF":-1,"SPD":-1,"state":-1,"x":-1,"y":-1}
         defaultData.update(data)
         data=defaultData
         for d in data:
             setattr(self,d,data[d])
 
-        checkDict=["act_state","act_id","name","image","race","job","HP_MAX","HP","MP_MAX","MP","STR","DEF","SPD","state",
+        checkDict=["actState","actId","name","image","race","job","HP_MAX","HP","MP_MAX","MP","STR","DEF","SPD","state",
                 "x","y"]
         if [hasattr(self,x) and str(type(getattr(self,x)))!="<class 'function'>" for x in checkDict] .count(False)!=0:
             raise ("necessary variable is not defined")
 
 class Enemy(Actor):
         def __init__(self,data={}):
-            defaultData = {"skill":["-1"]}
+            defaultData = {"tactics":{0:["attack",25],1:["heal",25],2:["throw",30],3:["beg",20]}}
             defaultData.update(data)
             data=defaultData
             Actor.__init__(self,data)
-
-            checkDict = ["skill"]
+            checkDict = ["tactics"]
             if [hasattr(self, x) and str(type(getattr(self, x))) != "<class 'function'>" for x in checkDict].count(
                     False) != 0:
                 raise (BaseException("necessary variables are not defined"))
@@ -50,7 +49,3 @@ class Enemy(Actor):
 
 class Player(Actor):
     pass
-
-param={"HP_MAX":1,"HP":1111111}
-s=Enemy(param)
-print(s.HP)
--- ActorController.py
+++ ActorController.py
@@ -1,61 +1,50 @@
 #coding:utf-8
 
 from Actor import Actor
+from Actor import Enemy
+import random as rnd
 class ActorController():
     def __init__(self):
+        self.actors={}
+        self.player=None
         pass
+
+    def setActorAsPlayer(self,actor):
+        self.player=actor
+
+    def addActor(self,actor):
+        l=[i for i in self.actors]
+        newId=-1
+        for n in range(len(l)+1):
+            if n not in l:
+                newId=n
+                break
+        actor.actId = newId
+        self.actors.update({newId: actor})
+
+    def delActor(self,delTarget): #actId or Actor どちらでも可
+        if type(delTarget)==int:
+            delId=delTarget
+        if type(delTarget)==Actor or Enemy or Plaer:
+            delId=delTarget.actId
+        self.actors.pop(delId)
 
 
     def action(self):
-        def isOpenWindow(key):
-            targetKeys = [K_z, K_c, K_v]
-            for targetKey in targetKeys:
-                print(pygame.key.name(targetKey),key[targetKey])
-                if key[targetKey]:
-                    return True
-            return False
-        def getKey():
-            while True:
-                pygame.time.wait(80)
-                res = pygame.event.get()
-                for event in res:
-                    if event.type == QUIT:
-                        pygame.quit()
-                        sys.exit()
-                        # pygame.key.set_repeat(300)
-                        #                    pygame.event.clear()
-                pressedKeys = pygame.key.get_pressed()
-                pressedMods = pygame.key.get_mods()
-                if np.sum(pressedKeys) > bin(pressedMods).count("1"):
-                    break
-            return pressedKeys
-        p=-1
-        actors.sort(key=lambda actor: actor.SPD)
-        for actor in actors:
-            if actor.name=="Player":
-                p=actor
-                break
-        assert p != -1, "Player not found"
-        for actor in actors:
-            if actor.act_state==WAIT_KEY:# if actor is player
-                while True:
-                    pressedKeys=getKey()
-                    if isOpenWindow(pressedKeys):
-                        isActFinish=True # doSomething
-                        if isActFinish:
-                            break
-                    else:
-                        isActFinish = actor.action(pressedKeys)
-                        if isActFinish:
-                            break
+        for actor in self.actors.values():
+            tactics = [actor.tactics[i][0] for i in actor.tactics]
+            weights=[actor.tactics[i][1] for i in actor.tactics]
+            print(rnd.choices(tactics,weights))
+        pass
 
-                stage.update()
-            if actor.act_state==WAIT:
-                actor.action(p)
-        stage.draw()
-        for actor in actors:
-            if actor!=p:
-                stage.drawHP(actor, p)
-        pygame.display.update()
+param={"HP":1,"job":"test"}
+actCtr=ActorController()
+actCtr.addActor(Enemy(param))
+actCtr.addActor(Enemy(param))
+actCtr.addActor(Enemy(param))
+actCtr.addActor(Enemy(param))
+actCtr.addActor(Enemy(param))
+actCtr.addActor(Enemy(param))
 
-
+actCtr.action()
+print([x for x in actCtr.actors])
